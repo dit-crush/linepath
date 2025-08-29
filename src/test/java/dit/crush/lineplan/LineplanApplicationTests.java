b6d31e4b94df2af4841a48ab3d1273967dd72459
ditcrush <289782449@qq.com>
2025-08-29 13:48:51 +0800

    Initial commit: add Spring Boot + Vue projects

---
+++ src/test/java/dit/crush/lineplan/LineplanApplicationTests.java
@@ -0,0 +1,13 @@
+package dit.crush.lineplan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LineplanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
